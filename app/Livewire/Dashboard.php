39c24961a7d97c8b6ff89370245b14b64aed24a7
Arif Budiman Arrosyid <arifbudimanarrosyid@gmail.com>
2025-06-13 02:39:38 +0700

    wip

--- app/Livewire/Dashboard.php
+++ app/Livewire/Dashboard.php
@@ -2,11 +2,14 @@
 
 namespace App\Livewire;
 
+use App\Models\Coupon;
 use App\Models\Program;
 use Livewire\Component;
+use Illuminate\Support\Str;
 use Livewire\Attributes\Title;
 use Livewire\Attributes\Layout;
 use Livewire\Attributes\Computed;
+use Illuminate\Support\Facades\Auth;
 
 class Dashboard extends Component
 {
@@ -15,6 +18,21 @@ class Dashboard extends Component
     public function programs()
     {
         return Program::get();
+    }
+
+    public function createCoupon(Program $program)
+    {
+        if (Auth::user()->coupons()->where('program_id', $program->id)->exists()) {
+            return;
+        }
+
+        Coupon::create([
+            'user_id' => Auth::id(),
+            'program_id' => $program->id,
+            'code' => Str::random(10),
+            'qr_code' => null,
+            'used_at' => null,
+        ]);
     }
 
     #[Layout('components.layouts.app')]
--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -1,22 +1,32 @@
-<div>
+<div class="space-y-6">
     <flux:heading size="xl" level="1">Selamat datang, {{ auth()->user()->name }}.</flux:heading>
-    @if (auth()->user()->coupons->count() === 0 )
+    {{-- @if (auth()->user()->coupons->count() === 0 )
         <flux:text class="mt-2 mb-6 text-base">Segera buat kupon.</flux:text>
     @else
         <flux:text class="mt-2 mb-6 text-base">Kupon bisa digunakan pada tanggal 13 - 06 - 2025</flux:text>
-    @endif
+    @endif --}}
 
-    <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
+    <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
         @foreach ($this->programs as $program)
         <x-card class="space-y-6">
             <flux:heading size="xl">
                 {{ $program->name }}
             </flux:heading>
+
             <flux:subheading>
                 {{ $program->description }}
             </flux:subheading>
 
-            <flux:button variant="primary">Create Coupon</flux:button>
+            @php
+                $coupon = auth()->user()->coupons
+                    ->firstWhere('program_id', $program->id);
+            @endphp
+
+            @if (!$coupon)
+                <flux:button variant="primary" wire:click="createCoupon({{ $program->id }})">Create coupon</flux:button>
+            @elseif (is_null($coupon->used_at))
+                <flux:button variant="primary">Print</flux:button>
+            @endif
         </x-card>
         @endforeach
     </div>
